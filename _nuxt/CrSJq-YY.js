d5d6c42deb1d080ebcdb347cf8a2487a27afafc6
Harsh Narayan Jha <harshnj@proton.me>
2025-05-17 09:11:59 +0530

    update build

---
+++ _nuxt/CrSJq-YY.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./D4l86DI8.js";import{q as n,o as s,w as f,h as p}from"./CfnoYObx.js";const l={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return s(),n(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{l as default};
